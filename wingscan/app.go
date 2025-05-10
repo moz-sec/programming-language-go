91527b757617b6e6c9509c978de38716046e98d8
moz-sec <m0253c@gmail.com>
2025-05-10 17:48:24 +0900

    feat: randomly scan ports

--- wingscan/app.go
+++ wingscan/app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"math/rand"
 	"net"
 	"time"
 )
@@ -39,7 +40,14 @@ func (a *App) ScanPort(host string, port int) (bool, error) {
 // Scans multiple ports on a given host
 func (a *App) ScanPorts(host string, startPort, endPort int) map[int]bool {
 	results := make(map[int]bool)
-	for port := startPort; port <= endPort; port++ {
+
+	ports := make([]int, endPort-startPort+1)
+	for i := range ports {
+		ports[i] = startPort + i
+	}
+	rand.Shuffle(len(ports), func(i, j int) { ports[i], ports[j] = ports[j], ports[i] })
+
+	for _, port := range ports {
 		isOpen, _ := a.ScanPort(host, port)
 		results[port] = isOpen
 	}
